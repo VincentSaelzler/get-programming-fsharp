a2caf9091fe42d6a1eda1df3f2a345d664a223c0
Vincent Saelzler <vincent.saelzler@gmail.com>
2022-12-19 22:36:09 -0500

    exhaustive checking

--- 5-20 Type System | Program Flow.fsx
+++ 5-20 Type System | Program Flow.fsx
@@ -1,7 +1,6 @@
-
-//  //  // 20.1 a tour around loops
 open System
 
+//  //  // 20.1 a tour around loops
 for number in 1 .. 10 do
     printfn "%d Hello!" number
 
@@ -29,6 +28,28 @@ while size = "small" do
 
 let thisr = r.Next()
 
-// 20.1.3 comprehensions
+// 5-20.1.3 Type System | Program Flow | Loops | Comprehensions
 let arrayOfChars = [| for c in 'a' .. 'z' -> Char.ToUpper c |]
 let listOfSquares = [ for n in 1 .. 5 -> n * n ]
+let seqOfStrings = seq {for i in 2 .. 4 .. 20 -> sprintf "number %d" i}
+let listOfStrings = List.ofSeq seqOfStrings
+
+// 5-20.2.3 Type System | Program Flow | Branching | Exhaustive Checking
+let getCreditLimit customer =
+    match customer with
+    "medium", 1 -> 500
+    | "good", 0 | "good", 1 -> 750
+    | "good", 2 -> 1000
+    | "good", years when years < 6 -> 2000
+    | "good", _ -> 4000
+    |_ -> 250 //if this line is commented, a compiler warning appears: "Incomplete pattern matches on this expression."
+    // the compiler warning identifies overlooked possibilities to prevent runtime errors.
+    // the compiler also warns if there are unreachable patterns. matching occurs from the top down.
+
+let m1 = getCreditLimit("medium", 1)
+let g0 = getCreditLimit("good", 0)
+let g1 = getCreditLimit("good", 1)
+let g2 = getCreditLimit("good", 2)
+let g3 = getCreditLimit("good", 3)
+let g9 = getCreditLimit("good", 9)
+let b0 = getCreditLimit("bad", 0) // Microsoft.FSharp.Core.MatchFailureException: The match cases were incomplete
