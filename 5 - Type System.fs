0ed932b47bd50d11fe606415ae5cc45db302329a
Vincent Saelzler <vincent.saelzler@gmail.com>
2022-12-25 22:50:48 -0500

    comment at storage device level

--- 5 - Type System.fs
+++ 5 - Type System.fs
@@ -5,9 +5,29 @@ type MMCType =
     | MMCPlus
     | SecureMMC
 
-type DiskType =
-    | Solid
+type StorageTechnology =
+    | SolidState
     | HardDisk of RPM:int * Platters:int
-    | MMC of MMCType * numPins:int
+    | MultimediaCard of MMCType * numPins:int
 
-let main () = "doing something"
+type StorageDevice = {
+    SizeGB:int
+    Technology:StorageTechnology
+}
+
+let commentOnMMCType mmcType =
+    match mmcType with
+    | SecureMMC -> false, "using it to be secure"
+    | RsMMC -> true, "why would you use reduced size?"
+    | MMCPlus -> true, "the extra size is worthless. use an ssd"
+
+let commentOnStorageTechnology storageTechnology =
+    match storageTechnology with
+    | SolidState -> false, "SSDs are good"
+    | HardDisk(_,_) ->  true, "don't use hard drives"
+    | MultimediaCard(mmcType,_) -> commentOnMMCType mmcType 
+
+let commentOnStorageDevice storageDevice =
+    match storageDevice with
+    | {Technology = HardDisk(5400,_); SizeGB = sizeGB} when sizeGB >= 8000-> false, "using an 8TB+ NAS drive is ok" 
+    | {Technology = technology } -> commentOnStorageTechnology technology
--- Program.fs
+++ Program.fs
@@ -4,6 +4,20 @@ open Five
 
 [<EntryPoint>]
 let main args =
-    printfn "%A" (Five.main())
+
+    printfn "%A" (Five.commentOnStorageTechnology(MultimediaCard(SecureMMC,4)))
+    printfn "%A" (Five.commentOnStorageDevice {
+        SizeGB = 6000
+        Technology = HardDisk(5400, 2)
+    } )
+    printfn "%A" (Five.commentOnStorageDevice {
+        SizeGB = 10000
+        Technology = HardDisk(7200, 2)
+    } )
+    printfn "%A" (Five.commentOnStorageDevice {
+        SizeGB = 10000
+        Technology = HardDisk(5400, 2)
+    } )
+
     ignore (Console.ReadLine())
     0
