31cf944d62ac0395da0e8a916647e321611183b4
Vincent Saelzler <vincent.saelzler@gmail.com>
2022-12-25 20:11:57 -0500

    cleanup merge mess

--- 5 - Type System.fs
+++ 5 - Type System.fs
@@ -9,16 +9,6 @@ type MMCType =
 type DiskType =
     | Solid
     | HardDisk of RPM:int * Platters:int
-<<<<<<< HEAD
     | MMC of MMCType * numPins:int
 
-
 // module Five
-
-
-
-
-
-=======
-    | MMC of MMCSubtype * numPins:int
->>>>>>> 4d2b01b10ef6d1f89f2f6b09f111200a83b1687f
--- main.fs
+++
@@ -1,7 +0,0 @@
-module Notes.Main
-open System
-[<EntryPoint>]
-let main args =
-    printfn "Arguments passed to function : %A" args
-    // Return 0. This indicates success.
-    0
