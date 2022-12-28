c04a5e2930d23145bb7f3cd6995b759d48b17ba0
Vincent Saelzler <vincent.saelzler@gmail.com>
2022-12-27 21:00:12 -0500

    lesson 21 on type system module done

--- 5 - Type System.fs
+++ 5 - Type System.fs
@@ -31,3 +31,22 @@ let commentOnStorageDevice storageDevice =
     match storageDevice with
     | {Technology = HardDisk(5400,_); SizeGB = sizeGB} when sizeGB >= 8000-> false, "using an 8TB+ NAS drive is ok" 
     | {Technology = technology } -> commentOnStorageTechnology technology
+
+let commentOnStorageDevices storageDevices =
+    
+    // put all the individual comments into a single string
+    let getstorageDevicesComment = 
+        List.map commentOnStorageDevice
+        >> List.map (fun sdc -> snd sdc)
+        >> List.distinct
+        >> String.concat ", "
+    // confirm if any of the devices have an issue
+    let getStorageDevicesIssue =
+        List.map commentOnStorageDevice
+        >> List.exists (fun sdc -> fst sdc )
+
+    match storageDevices with
+    | [] -> true, "at least one drive is required"
+    | [ {Technology = SolidState } ] -> false, "single SSD setup. cool - very normal"
+    | [ { SizeGB = 128; Technology = SolidState }; { SizeGB = 2000; Technology = SolidState } ] -> false, "that's my PC!"
+    | storageDevices -> (getStorageDevicesIssue storageDevices, getstorageDevicesComment storageDevices)
--- Program.fs
+++ Program.fs
@@ -4,20 +4,50 @@ open Five
 
 [<EntryPoint>]
 let main args =
+    // technology
+    printfn "%A" (commentOnStorageTechnology(MultimediaCard(SecureMMC,4)))
 
-    printfn "%A" (Five.commentOnStorageTechnology(MultimediaCard(SecureMMC,4)))
-    printfn "%A" (Five.commentOnStorageDevice {
+    // device
+    printfn "%A" (commentOnStorageDevice {
         SizeGB = 6000
         Technology = HardDisk(5400, 2)
     } )
-    printfn "%A" (Five.commentOnStorageDevice {
+    printfn "%A" (commentOnStorageDevice {
         SizeGB = 10000
         Technology = HardDisk(7200, 2)
     } )
-    printfn "%A" (Five.commentOnStorageDevice {
+    printfn "%A" (commentOnStorageDevice {
         SizeGB = 10000
         Technology = HardDisk(5400, 2)
     } )
+
+    // devices
+    printfn "no drives: %A" (commentOnStorageDevices [] )
+
+    printfn "single ssd: %A" (commentOnStorageDevices [
+        { SizeGB = 2000; Technology = SolidState }
+    ] )
+
+    printfn "my pc: %A" (commentOnStorageDevices [
+        { SizeGB = 128; Technology = SolidState }; { SizeGB = 2000; Technology = SolidState }
+    ] )
+
+    printfn "bad drives: %A" (commentOnStorageDevices [
+        { SizeGB = 256; Technology = SolidState }
+        { SizeGB = 128; Technology = SolidState }
+        { SizeGB = 10000; Technology = HardDisk(5400, 2) }
+        { SizeGB = 32 ; Technology = MultimediaCard(SecureMMC,4)}
+        { SizeGB = 32 ; Technology = MultimediaCard(RsMMC,4)}
+        { SizeGB = 2000; Technology = HardDisk(5400, 2) }
+    ] )
+
+    printfn "good drives: %A" (commentOnStorageDevices [
+        { SizeGB = 256; Technology = SolidState }
+        { SizeGB = 128; Technology = SolidState }
+        { SizeGB = 10000; Technology = HardDisk(5400, 2) }
+    ] )
+
+
 
     ignore (Console.ReadLine())
     0
